a3117be05e92f34bcf3484ddcc327d91b92c4562
RenanCarbosGti <renancesar.gti@gmail.com>
2026-06-01 14:07:02 -0300

    corrige acesso

--- config/conexao.php
+++ config/conexao.php
@@ -1,28 +1,31 @@
-<?php
-// config/conexao.php
-// Padrão idêntico ao projeto_oo: classe estática com conectar() e desconectar()
-
 class conexao
 {
     private static $dbHost = "sql302.infinityfree.com";
-    private static $dbName = "if0_42065046_XXX";
+    private static $dbName = "if0_42065046_ac_manager";
     private static $dbUser = "if0_42065046";
     private static $dbPass = "Luiza2023Renan";
+
+    private static $con = null;
 
     public static function conectar()
     {
         if (self::$con == null) {
             try {
                 self::$con = new PDO(
-                    "mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName . ";charset=utf8mb4",
+                    "mysql:host=" . self::$dbHost .
+                    ";dbname=" . self::$dbName .
+                    ";charset=utf8mb4",
                     self::$dbUser,
                     self::$dbPass
                 );
+
                 self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             } catch (PDOException $exception) {
                 die("Erro de conexão: " . $exception->getMessage());
             }
         }
+
         return self::$con;
     }
 
